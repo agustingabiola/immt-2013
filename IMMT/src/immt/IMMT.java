4a5973b0befb5afd5d33c4fcbc042b82f52d3811
gastonclaret <gastonclaret@gmail.com>
2013-05-11 21:13:17 +0000

    Innitial Commmit. Imported IJ libraries. Basic shell window

---
+++ IMMT/src/immt/IMMT.java
@@ -0,0 +1,12 @@
+package immt;
+
+import immt.ui.ShellWindow;
+
+public class IMMT {
+
+    public static void main(String[] args) {
+        ShellWindow shell = new ShellWindow();
+        shell.show();
+    }
+
+}
---
+++ IMMT/src/immt/ui/ImagePanel.java
@@ -0,0 +1,47 @@
+package immt.ui;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Gaston
+ */
+public class ImagePanel extends JPanel {
+    
+    private BufferedImage image;
+    
+    /**
+    * Creates an ImagePanel which extends from JPanel
+    */
+    public ImagePanel(){
+    
+    }
+    
+    /**
+     * Creates an ImagePanel which extends from JPanel
+     * @param image Image to set inside the panel
+     */
+    public ImagePanel(BufferedImage image) {
+        this.image = image;
+    }
+    
+    /**
+     * Returns the BufferedImage inside the panel
+     * @return 
+     */
+    public BufferedImage getImage() {
+        return image;
+    }
+    
+    public void setImage(BufferedImage image){
+        this.image = image; 
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.drawImage(image, 0, 0, getSize().width, getSize().height, this);
+    }
+
+}
