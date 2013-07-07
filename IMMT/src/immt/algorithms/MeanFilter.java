09d7366a004fbef219e466285403591a41b4a115
agustin.gabiola <agustin.gabiola@gmail.com>
2013-07-07 23:54:58 +0000

    Tool Tips added and working

--- IMMT/src/immt/algorithms/MeanFilter.java
+++ IMMT/src/immt/algorithms/MeanFilter.java
@@ -12,12 +12,12 @@ public class MeanFilter extends Algorithm {
     private int radio;
 
     public MeanFilter(ShellWindow parent) {
-        super("Mean Filter", "The Mean Filter is a simple one, and does \n"
-                + "not remove the speckles but averages it into the data. Generally \n"
-                + "speaking, this is the least satisfactory method of speckle noise \n"
-                + "reduction as it results in loss of detail and resolution. However, \n"
-                + "it can be used for applications where resolution is not the first \n"
-                + "concern.", parent);
+        super("Mean Filter", "The Mean Filter is a simple one, and does not remove the \n"
+                + "speckles but averages it into the data. Generally speaking, this is \n"
+                + "the least satisfactory method of speckle noise reduction as it \n"
+                + "results in loss of detail and resolution. However, it can be used \n"
+                + "for applications where resolution is not the first concern.", parent);
+                
     }
 
     @Override
