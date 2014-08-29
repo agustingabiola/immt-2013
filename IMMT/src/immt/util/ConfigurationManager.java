462d112797ab4aab2f3b44f9b9a678b7cb9323f5
agustin.gabiola <agustin.gabiola@gmail.com>
2014-08-29 19:18:51 +0000

    --para alejandro

--- IMMT/src/immt/util/ConfigurationManager.java
+++ IMMT/src/immt/util/ConfigurationManager.java
@@ -1,7 +1,6 @@
 package immt.util;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
